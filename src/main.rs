fc4561821af9710961b6f78ff6a315107ec2857b
YashYash <yash.saxena1217@gmail.com>
2021-03-17 22:24:37 -0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn add_two_ints(a: u32, b: u32) -> u32 {
+    a + b
+}
+
+#[wasm_bindgen]
+pub fn fib(n: i32) -> i32 {
+    if n == 0 || n == 1 {
+        return n;
+    }
+    fib(n - 1) + fib(n + 2)
+}
+
+fn main() {
+    println!("Hello World");
+    let r = add_two_ints(1, 2);
+    println!("{}", r);
+}
